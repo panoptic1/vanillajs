cec0ba4794bc786c0837fe5c0d6d48e216face89
Ryan Helgerson <ryan.helgerson@gmail.com>
2020-05-11 11:52:49 -0500

    query selector practice

---
+++ logic.js
@@ -0,0 +1,13 @@
+//Day 1
+//Getting an element from the DOM with querySelector
+//The first div...
+const elem = document.querySelector('div');
+console.log(elem);
+
+//The first heading...
+const heading = document.querySelector('#greeting');
+console.log(heading);
+
+//The first paragraph...
+const paragraph = document.querySelector('.lorem');
+console.log(paragraph);
