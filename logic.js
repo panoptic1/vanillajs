e6b63c9671c4593c778ad27cc8fb6be139080267
Ryan Helgerson <ryan.helgerson@gmail.com>
2020-05-11 14:53:19 -0500

    event listener added to button

--- logic.js
+++ logic.js
@@ -1,13 +1,22 @@
 //Day 1
 //Getting an element from the DOM with querySelector
 //The first div...
-const elem = document.querySelector('div');
-console.log(elem);
+// const elem = document.querySelector('div');
+// console.log(elem);
 
-//The first heading...
-const heading = document.querySelector('#greeting');
-console.log(heading);
+// The first heading...
+// const heading = document.querySelector('#greeting');
+// console.log(heading);
 
-//The first paragraph...
-const paragraph = document.querySelector('.lorem');
-console.log(paragraph);
+// The first paragraph...
+// const paragraph = document.querySelector('.lorem');
+// console.log(paragraph);
+
+//Add an event listener to the button
+// const btn = document.querySelector('#click-me-1');
+// //console.log(btn);
+// btn.addEventListener('click', function(event){
+//     console.log("You pushed the button!");
+//     console.log(event); //the event details
+//     console.log(event.target); //the clicked element
+// }, false);
