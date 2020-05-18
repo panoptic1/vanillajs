52420071c90a73ebc60db913336ace8e5f5f25d2
Ryan Helgerson <ryan.helgerson@gmail.com>
2020-05-17 19:25:33 -0500

    Solution after converting data type to string

--- day7/solution.js
+++ day7/solution.js
@@ -1,14 +1,17 @@
-//console.log("let's go!")
-
 //create an event listener for text input anywhere in the document
 document.addEventListener('input', function(event){
+    
     //declare a variable that will target the input inside the textarea
     let text = document.querySelector('#text').value
-    //console.log(text);
-    console.log(text.length);
-})
+    //set the text variable to the length of the input
+    text = text.length;
+    //convert the value for text from a number to a string
+    text = text.toString();
+    
+    //declare variable that targets the text content of the span
+    let count = document.querySelector('#character-count');
+    
+    //print a new value to span by setting textContent to text
+    count.textContent = text
 
-    
-    
-    
-//VARIABLES
+})
