7881a386656239bbe0a7118206329ec1bb02428e
Ryan Helgerson <ryan.helgerson@gmail.com>
2020-05-19 13:13:23 -0500

    character counter working

--- day9/solution.js
+++ day9/solution.js
@@ -1,5 +1,25 @@
 //CHALLENGE: Write a script that calculates both the number of characters and the number of words that are input
 //into a textarea
 
-//VARIABLES++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//declare variables to target the <textarea> element as well as both of the <span> elements
+//declare an event listener to listen for any text input
+document.addEventListener('input', function(event){
+    //declare a variable called 'characters' to target the total characters logged into the <textarea> element
+    //follow the day 7 solution in order to accomplish this
+    let characters = document.querySelector('#text').value.length
+    //console.log(characters);
+
+    //declare a variable called 'words' to target the number of words written into the <textarea> element by the user
+    //use the split method to determine how many words are in the textarea
+    //let words = document.querySelector('#text').split(' ')
+
+    //declare a variable called 'characterCount' that targets the span for the character counter
+    //set the value of 'characterCount.textContent' to 'characters'
+    let characterCount = document.querySelector('#character-count');
+    characterCount.textContent = characters
+
+    //declare a variable called 'wordCount' that targets the span for the word counter
+    //set the value of 'wordCount.textContent' to 'words'
+})
+
+
+
