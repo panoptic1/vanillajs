15c54f8283f284a4483be5faf2fb0fe931ab0922
Ryan Helgerson <ryan.helgerson@gmail.com>
2020-05-23 20:18:32 -0500

    I wrote a promise, baby!

--- randomRon/solution.js
+++ randomRon/solution.js
@@ -1,4 +1,4 @@
-//use fetch to have a look at the data object that comes back when you use fetch on the Ron Swanson API
+//use fetch to have a look at the data that comes back on the Ron Swanson API
 fetch('http://ron-swanson-quotes.herokuapp.com/v2/quotes').then(function (response){
     //the API call was successful!
     return response.json();
@@ -10,7 +10,18 @@ fetch('http://ron-swanson-quotes.herokuapp.com/v2/quotes').then(function (respon
 });
 
 //Write a variable for the 'More Ron' button
+const button = document.querySelector("button")
 
 //Create an event listener for a click event on the More Ron button
+button.addEventListener('click', function(event){
+    
+    //Write a promise that pings the Ron Swanson API and renders the data in the blockquote element
+    const getQuote = new Promise ( function (resolve, reject){
+        resolve("You wrote a promise!")
+    })
 
-//Write a promise that pings the Ron Swanson API and renders the data in the blockquote element
+    getQuote.then( function (quote){
+        console.log(quote);
+    })
+})
+
