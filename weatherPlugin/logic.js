eb6793da70cc5c2b8dd90a5fefa4b75831cd94c6
Ryan Helgerson <ryan.helgerson@gmail.com>
2020-06-18 10:46:36 -0500

    Can't access the settings object keys in render function.

--- weatherPlugin/logic.js
+++ weatherPlugin/logic.js
@@ -20,11 +20,37 @@ var createForecast = function (options) {
     var settings = Object.assign(defaults, options)
 
     var key = `b2f0c7d69dc446289736467fbe50c452`;
+
+    
+    /*!
+    * Sanitize and encode all HTML in a user-submitted string
+    * (c) 2018 Chris Ferdinandi, MIT License, https://gomakethings.com
+    * @param  {String} str  The user-submitted string
+    * @return {String} str  The sanitized string
+    */
+	var sanitizeHTML = function (str) {
+		var temp = document.createElement('div');
+		temp.textContent = str;
+		return temp.innerHTML;
+    };
+
     
     //define the renderWeather function using the variables that we're created in the default settings of the plugin
     var renderWeather = function(){
-        console.log(`Here's the weather, bitches!`)
+        console.log(settings);
+        var forecast = `
+        <h2>Current conditions in ` + sanitizeHTML(settings.city) + `</h2>
+        <p>The temperature is ` + sanitizeHTML(settings.fahrenheit) + `</p>
+        <p>Here's the wind: ` + sanitizeHTML(settings.windDirection) + `</p>
+        `
+        console.log(`City: ` , settings.city);
+        console.log(`Fahrenheit: ` , settings.fahrenheit);
+        console.log(`Wind Direction: `, settings.windDirection);
+        var display = document.querySelector(settings.selector);
+
+        display.innerHTML = forecast
     }
+
     //get json
     function getWeatherInfo(){
         //Call the API
@@ -57,14 +83,15 @@ var createForecast = function (options) {
                     
                     //set the value as the relevant index on the array that comes back from the api
                     data = data.data[0];
-                    //console.log(data);
+                    console.log(data);
 
                     //set the values for the weather-related keys in settings
                     settings.icon = data.weather.icon;
                     settings.description = data.weather.description;
                     settings.celsius = data.temp;
-                    settings.windDirection = data.win_cdir_full;
+                    settings.windDirection = data.wind_cdir_full;
                     settings.windSpeed = data.wind_spd;
+                    settings.fahrenheit = (settings.celsius * 9 / 5) + 32;
                     
                 }).then(renderWeather())
                 .catch(function (error) {
