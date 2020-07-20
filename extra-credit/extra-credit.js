569696eda12c23daf0f9bac63b7e49ddc1c77eb3
Ryan Helgerson <ryan.helgerson@gmail.com>
2020-07-19 21:18:02 -0500

    Epilogue

---
+++ extra-credit/extra-credit.js
@@ -0,0 +1,15 @@
+// Until then, here are some “extra credit” projects you might want to work on.
+
+// Convert the Show Password project to use a button instead a checkbox. Consider things like button state and screen readers.
+// Modify the Word Count project to show the user what words they use most frequently, and how often they’re used.
+// Build your own animated GIF search app using the Giphy API.
+// Let users select the NY Times article categories they want to get articles for.
+// Convert the Monsters! game into Minesweeper.
+// Modify the Table of Contents plugin to support multiple heading levels (warning: this one is a lot harder than it sounds).
+// Use the browser-native Geolocation API to get the user’s location for the Weather App.
+// Let users add multiple locations to the Weather App, and save them between visits.
+// Extend your DOM Library with helper functions that you find yourself using on lots of projects.
+// Convert the Form Saver script into a plugin. Give users the ability to save/delete their form data instead of doing it automatically.
+// Give users a way to set their own time for the Countdown Timer. Play a sound when the timer ends.
+// Tie into real location data for the Places app.
+// Add routing to the Places so that each place has its own page, with Google Map data and directions on how to get there.
